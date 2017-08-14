904021cd5f24008dd370b863f2c3c7b69cd6d2af
zwt <30827013+ZhuWenTong@users.noreply.github.com>
2017-08-15 00:14:51 +0800

    原生ajax请求

---
+++ ajax.js
@@ -0,0 +1,44 @@
+function ajax(opt) {
+	opt = opt || {};
+	opt.method = opt.method.toUpperCase() || 'POST';
+	opt.url = opt.url || '';
+	opt.async = opt.async || true;
+	opt.data = opt.data || null;
+	opt.success = opt.success || function() {};
+	var xmlHttp = null;
+	if (XMLHttpRequest) {
+		xmlHttp = new XMLHttpRequest();
+	} else {
+		xmlHttp = new ActiveXObject('Microsoft.XMLHTTP');
+	}
+	var params = [];
+	for (var key in opt.data) {
+		params.push(key + '=' + opt.data[key]);
+	}
+	var postData = params.join('&');
+	if (opt.method.toUpperCase() === 'POST') {
+		xmlHttp.open(opt.method, opt.url, opt.async);
+		xmlHttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded;charset=utf-8');
+		xmlHttp.send(postData);
+	} else if (opt.method.toUpperCase() === 'GET') {
+		xmlHttp.open(opt.method, opt.url + '?' + postData, opt.async);
+		xmlHttp.send(null);
+	}
+	xmlHttp.onreadystatechange = function() {
+		if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+			opt.success(xmlHttp.responseText);
+		}
+	};
+}
+//使用示例
+//ajax({
+//    method: 'POST',
+//    url: '',
+//    data: {
+//        data,
+//        ... 
+//    },
+//    success(res) {
+//       console.log(res);
+//    }
+//});
