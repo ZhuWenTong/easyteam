98492e8e57a5ac94923af802c02fb3a4880e543e
zwt <30827013+ZhuWenTong@users.noreply.github.com>
2017-08-14 18:11:22 +0800

    函数作为参数传入另一函数中调用的场景

--- callback.js
+++ callback.js
@@ -10,5 +10,5 @@ var obj = {
 	}
 }
 obj.init(obj.param,'zwt');
-//例如: 获取数据getData(封装好的函数)时标识丢失 需在此调用登录函数login(封装好)获取该标识 
+//eg: 获取数据getData(封装好的函数)时标识丢失 需在此调用登录函数login(封装好)获取该标识 
 //判断标识丢失 -> 将自身getData作为参数及获取数据需要的参数传入login中 -> login获取标识后即可调用传进来的getData.
