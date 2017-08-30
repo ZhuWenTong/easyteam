ac63a59f11e4e46c4588f35a0717fa4156850e84
zwt <30827013+ZhuWenTong@users.noreply.github.com>
2017-08-30 17:58:33 +0800

    es6 demo

---
+++ es6/字符串扩展/demo1.js
@@ -0,0 +1,4 @@
+let str = 'abc';
+for(let key of str){
+	console.log(key);//a b c
+}
---
+++ es6/字符串扩展/demo3.js
@@ -0,0 +1,6 @@
+//repeat() 返回一个新的字符串 表示将原字符串重复n次
+let str = 'zwt',
+	test = str.repeat(3);
+console.log(test);//zwtzwtzwt
+
+console.log(`hello ${str}`);
