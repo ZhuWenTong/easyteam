ff5153e36986d6090b8ac4eec7f8d91dbc294295
zwt <30827013+ZhuWenTong@users.noreply.github.com>
2018-08-21 10:38:23 +0800

    js处理对象的一些操作

--- object.js
+++ object.js
@@ -1,64 +1,64 @@
 var a = [{
-	id: 1,
-	name: 1
+    id: 1,
+    name: 1
 }, {
-	id: 2,
-	name: 2
+    id: 2,
+    name: 2
 }, {
-	id: 3,
-	name: 4
+    id: 3,
+    name: 4
 }];
 var result = a.find((a) => {
-	console.log(a)
-	if (a.id == 3) {
-		return a;
-	}
+    console.log(a)
+    if (a.id == 3) {
+        return a;
+    }
 });
 console.info(result);
 
 result = a.filter((a) => {
-	console.log(a)
-	if (a.id == 2) {
-		return a;
-	}
+    console.log(a)
+    if (a.id == 2) {
+        return a;
+    }
 });
 console.info(result[0]);
 
 a.forEach((a) => {
-	console.log(a)
-	if (a.id == 3) {
-		result = a;
-		return;
-	}
+    console.log(a)
+    if (a.id == 3) {
+        result = a;
+        return;
+    }
 });
 
 console.info(result);
 
 a.map((a) => {
-	console.log(a)
-	if (a.id == 2) {
-		result = a;
-		return;
-	}
+    console.log(a)
+    if (a.id == 2) {
+        result = a;
+        return;
+    }
 });
 console.log(result);
 
 a.some((a) => {
-	if (a.id == 3) {
-		result = a;
-		return;
-	}
+    if (a.id == 3) {
+        result = a;
+        return;
+    }
 });
 
 console.log(result);
 
 var b = a.entries(),
-	c;
+    c;
 while (!(c = b.next()).done) {
-	if (c.id == 3) {
-		result = c;
-		break;
-	}
+    if (c.id == 3) {
+        result = c;
+        break;
+    }
 }
 console.log(result);
 
@@ -66,7 +66,6 @@ console.log(result);
 var obj = [{ "name": "xiaoming" }, { "name": "" }, { "name": "zhangsan" }];
 for (var key in obj) {
     if (obj[key]["name"] == "") {
-
         obj[key]["name"] = "0";
     }
 }
@@ -74,13 +73,13 @@ console.dir(obj);
 
 //将多个对象合并成一个
 var obj1 = {
-	a: 1
-},
-	obj2 = {
-		b: 2
-	},
-	obj3 = {
-		c: 3
-	};
-var obj = Object.assign(obj1,obj2,obj3);
-console.log(obj);//{a:1,b:2,c:3}
+        a: 1
+    },
+    obj2 = {
+        b: 2
+    },
+    obj3 = {
+        c: 3
+    };
+var obj = Object.assign(obj1, obj2, obj3);
+console.log(obj); //{a:1,b:2,c:3}
